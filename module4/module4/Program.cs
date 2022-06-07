131c788f5d248253225b04071f9d0a3226ffa1c4
antonovxx <80815609+antonovxx@users.noreply.github.com>
2022-06-07 21:35:45 +0300

    module4 is DONE

---
+++ module4/module4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+/* Задание 1. Случайная матрица
+
+ * Что нужно сделать:
+Выведите на экран матрицу заданного размера и заполните её случайными числами. Детальный алгоритм работы приложения:
+
+Сначала пользователю предлагается ввести количество строк в будущей матрице.
+Затем — ввести количество столбцов в будущей матрице.
+После того, как данные будут получены, нужно создать в памяти матрицу заданного размера.
+Используя Random, заполнить матрицу случайными целыми числами.
+Вывести полученную матрицу на экран. 
+Вывести суммы всех элементов этой матрицы на экран отдельной строкой. 
+ */
+
+namespace task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Rows: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Cols: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+
+            int[,] matrix = new int[rows, cols];
+
+            int sum = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = random.Next(0, 10);
+                    sum += matrix[i, j];
+                }
+            }
+
+            for(int i = 0; i < cols; i++)
+            {
+                for(int j = 0; j < rows; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+
+            Console.WriteLine("Sum of the whole elements: " + sum);
+        }
+    }
+}
---
+++ module4/task2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+/*
+ * Задание 2. Наименьший элемент в последовательности
+ 
+ * Что нужно сделать
+Найдите наименьший элемент в последовательности, которую вводит пользователь. Последовательность нужно сохранить в массив. 
+Детальный алгоритм программы:
+
+Пользователь вводит длину последовательности. 
+Затем пользователь последовательно вводит целые числа (как положительные, так и отрицательные). Числа разделяются клавишей Enter.
+Каждое введённое число помещается в соответствующий элемент массива.
+После окончания ввода данных отдельный цикл проходит по последовательности и находит минимальное число. 
+Для этого он сравнивает каждое число в итерации с предыдущим найденным минимальным числом. 
+
+ */
+namespace task2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of the array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int [] array = new int[size];
+
+            Console.WriteLine("Please, fill the array: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            
+            int min = MinValue(array);
+
+            Console.WriteLine("Array: ");
+
+            foreach(int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\nMinimal value in the collection is " + min);
+            Console.WriteLine("Maximum value in the collection is " + array.Max());
+        }
+
+        public static int MinValue(int[] array)
+        {
+            int min = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+    }
+}
