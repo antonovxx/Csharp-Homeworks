00d9d3cdf7738873ac9ee20edcfbb5c8ef023cde
antonovxx <80815609+antonovxx@users.noreply.github.com>
2022-06-10 12:02:07 +0300

    Add files via upload

---
+++ module5/task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+/*Задание 1. Метод разделения строки на слова
+    * Что нужно сделать
+    Пользователь вводит в консольном приложении длинное предложение. 
+    Каждое слово в этом предложении отделено одним пробелом. 
+    Необходимо создать метод, который в качестве входного параметра принимает строковую переменную,
+    а в качестве возвращаемого значения — массив слов.
+    После вызова данного метода программа вызывает второй метод, который выводит каждое слово в отдельной строке.   
+ */
+
+namespace task1
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Разделение целой строки на подстроку
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string[] SplitString(string str)
+        {
+            string[] strArr = str.Split(' ');
+            return strArr;
+        }
+        /// <summary>
+        /// Вывод массива в консоль
+        /// </summary>
+        /// <param name="str"></param>
+        public static void PrintString(string[] str)
+        {
+            foreach(string word in str)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some sentence: ");
+            string str = Console.ReadLine();
+
+            string [] strArr = SplitString(str);
+            PrintString(strArr);
+        }
+    }
+}
---
+++ module5/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+/*Задание 2. Перестановка слов в предложении
+    * Что нужно сделать
+    Пользователь вводит в программе длинное предложение. Каждое слово раздельно одним пробелом. 
+    После ввода пользователь нажимает клавишу Enter. Необходимо создать два метода:
+    * первый метод разделяет слова в предложении;
+    * второй метод меняет эти слова местами (в обратной последовательности). 
+    При этом важно учесть, что один метод вызывается внутри другого метода,
+    то есть в методе main вызывается метод cо следующей сигнатурой — ReversWords (string inputPhrase). 
+    Внутри этого метода вызывается метод по разделению входной фразы на слова.
+*/
+
+namespace task2
+{
+    public class Program
+    {
+        public static string SplitString(string str)
+        {
+            string[] strArr = str.Split(' ');
+            string newStr = ReverseString(strArr);
+            return newStr;
+        }
+        public static string ReverseString(string[] strArr)
+        {
+            Array.Reverse(strArr);
+            string str = string.Join(" ", strArr);
+            return str;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some string: ");
+            string str = Console.ReadLine();
+
+            str = SplitString(str);
+            Console.WriteLine(str);
+        }
+    }
+}
